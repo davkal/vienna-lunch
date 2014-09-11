d067a6d511c32ff485ff1afa0a67e40afe8a78c8
David Kaltschmidt <david.kaltschmidt@gmail.com>
2014-09-11 23:31:53 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+ViennaHackerLunch::Application.config.secret_token = 'b6d0aab6e1a78dd20cd85b2a91087cf54066d9d13c0ca540fa67487aa17d0d1113ec3271a27ac6bca90833633d1c43b0cc58c8d15d1a9b27f9a84af4d100fa38'
