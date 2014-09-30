e7dea7a62b6672b2695790ef4c3ce43dfc2240f0
David Kaltschmidt <david.kaltschmidt@gmail.com>
2014-09-30 12:34:05 +0200

    added restaurant Podium

--- lib/vienna_lunch/restaurants.rb
+++ lib/vienna_lunch/restaurants.rb
@@ -1,14 +1,22 @@
 module ViennaLunch::Restaurants
   GLOB = "lib/vienna_lunch/restaurants/*.rb"
+  LIST = [
+    ViennaLunch::Restaurants::Podium
+  ]
 
   def self.load_all
-    return [ViennaLunch::Restaurants::Podium]
+    #Dir[GLOB].each do |file|
+    #  require file
+    #  file_name = File.basename(file, '.rb')
+    #  file_name.camelcase.constantize.new
+    #end
 
-
-    Dir[GLOB].each do |file|
-      require file
-      file_name = File.basename(file, '.rb')
-      file_name.camelcase.constantize.new
+    return LIST.map do |restaurant|
+      {
+        name: restaurant::NAME,
+        location: restaurant::LOCATION,
+        today: restaurant::today,
+      }
     end
   end
 end
--- lib/vienna_lunch/restaurants/podium.rb
+++ lib/vienna_lunch/restaurants/podium.rb
@@ -1,10 +1,19 @@
-#require 'vienna_lunch/namespace'
+require 'nokogiri'
+require 'open-uri'
+
 
 module ViennaLunch::Restaurants::Podium
-  ADDRESS = "Westbahnstr. 150, 1070 Wien"
+  NAME = "Podium"
+  ADDRESS = "Westbahnstrasse 33, 1070 Wien"
   LOCATION = ""
-
+  MENU_URL = 'http://www.restaurant-podium.at/menue.html'
+  
   def self.today
-    "Dish of the day"
+    doc = Nokogiri::HTML(open(MENU_URL))
+
+    meat = doc.xpath("//table[4]//font[1]/text()")
+    vegetarian = doc.xpath("//table[7]//font[1]/text()")
+
+    "#{meat} or #{vegetarian}"
   end
 end
