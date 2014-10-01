1538bdf9dbf8fe253d56301d8a31aa4b26e6929a
David Kaltschmidt <david.kaltschmidt@gmail.com>
2014-10-01 17:15:34 +0200

    Rails.cache-based cache for restaurant list
    actionpack-action_caching is broken with rails-api

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,7 +2,13 @@
 
 class RestaurantsController < ApplicationController
   def index
-    @restaurants = ViennaLunch::Restaurants::load_all
+    @restaurants = cached_restaurants
     render json: @restaurants
+  end
+
+  def cached_restaurants
+    Rails.cache.fetch('restaurants_list', :expires_in => 10.minutes) do
+      ViennaLunch::Restaurants::load_all
+    end
   end
 end
