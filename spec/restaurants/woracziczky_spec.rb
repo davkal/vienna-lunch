1b68452bc33e7bab6aee44980d8365ce3375d723
David Kaltschmidt <david.kaltschmidt@gmail.com>
2015-03-13 12:19:37 +0100

    fix for woracziczky

--- spec/restaurants/woracziczky_spec.rb
+++ spec/restaurants/woracziczky_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
   describe ViennaLunch::Restaurants::Woracziczky do
   it 'should have todays lunch' do
     @lunch = ViennaLunch::Restaurants::Woracziczky::lunch
-    expect(@lunch).to match(/(menü|heut|mittag|montag|dienstag|mittwoch|donnerstag|freitag)/i)
+    expect(@lunch).to match(/(menü|heut|mittag|montag|dienstag|mittwoch|donnerstag|freitag|wochenende)/i)
   end
 end
