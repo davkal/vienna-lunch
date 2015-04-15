f021993374e7cfaaec29471ebaf908c1ed375bc9
David Kaltschmidt <david.kaltschmidt@gmail.com>
2015-04-15 11:47:57 +0200

    fixed wirr

--- lib/vienna_lunch/restaurants/wirr.rb
+++ lib/vienna_lunch/restaurants/wirr.rb
@@ -10,8 +10,27 @@ module ViennaLunch::Restaurants::Wirr
   def self.lunch
     doc = Nokogiri::HTML(open(MENU_URL))
 
-    weekday_today = Date.today.wday # skipping first <p>
-    todays_menu = doc.css('.post-excerpt p')[weekday_today]
+    # first two 2 p are empty
+    # p[2-5] are monday
+    # p[6-9] are tuesday
+    # p[10-13] are wednesday
+    # ...
+    #
+    # hence if weekday is n then we can get
+
+
+    weekday_today = Date.today.wday - 1  # monday should be 0
+    span = 4 # starter, veg, meat, dessert
+    offset = weekday_today * span + 2
+    paragraphs = doc.css('.post-excerpt p')
+    todays_menu = ""
+    todays_menu << paragraphs[offset]
+    todays_menu <<  "\n"
+    todays_menu << paragraphs[offset + 1]
+    todays_menu <<  "\n"
+    todays_menu << paragraphs[offset + 2]
+    todays_menu <<  "\n"
+    todays_menu << paragraphs[offset + 3]
     if todays_menu
       return todays_menu.to_s
     else
--- spec/restaurants/wirr_spec.rb
+++ spec/restaurants/wirr_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
   describe ViennaLunch::Restaurants::Wirr do
   it 'should have todays lunch' do
     @lunch = ViennaLunch::Restaurants::Wirr::lunch
-    expect(@lunch).to match(/II/i)
+    expect(@lunch).to match(/II /i)
+    expect(@lunch).to match(/- /i)
   end
 end
