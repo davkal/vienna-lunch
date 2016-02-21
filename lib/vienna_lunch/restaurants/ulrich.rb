0d3a231be09638949d24c0d1dcf8700d3994b128
David Kaltschmidt <david.kaltschmidt@gmail.com>
2016-02-21 20:43:02 +0100

    Fix ulrich new mittagsmenu

--- lib/vienna_lunch/restaurants/ulrich.rb
+++ lib/vienna_lunch/restaurants/ulrich.rb
@@ -7,22 +7,22 @@ module ViennaLunch::Restaurants::Ulrich
   ADDRESS = "St. Ulrichplatz 1, 1070 Wien"
   LOCATION = [48.2047882,16.3528004]
   URL = 'http://ulrichwien.at/'
-  MENU_URL = 'http://ulrichwien.at/speisekarte/'
+  MENU_URL = 'http://ulrichwien.at/'
 
-  WEEKDAYS = ['MONTAG', 'DIENSTAG', 'MITTWOCH', 'DONNERSTAG', 'FREITAG']
+  WEEKDAYS = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag']
 
   def self.lunch
     doc = Nokogiri::HTML(open(MENU_URL))
 
     food = ''
-    first_tab = doc.css('.tab-content').first
+    first_tab = doc.css('#mittagsmenu .entry-content-wrapper').first
 
     # find today
     weekday_today = Date.today.wday - 1 # starting week on monday
-    first_tab.css('.menu-posts').each do |post|
-      weekday = post.css('.category-title h2').text
+    first_tab.css('.flex_column').each do |post|
+      weekday = post.css('h3').text
       if WEEKDAYS.index(weekday) == weekday_today
-        food = post.css('.menu-title').map{ |el| el.text }.join('<br>')
+        food = weekday + ': ' + post.css('section p').map{ |el| el.text }.join('<br>')
       end
     end
 
--- spec/restaurants/ulrich_spec.rb
+++ spec/restaurants/ulrich_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
   describe ViennaLunch::Restaurants::Ulrich do
   it 'should have todays lunch' do
     @lunch = ViennaLunch::Restaurants::Ulrich::lunch
-    expect(@lunch).to match(/mit/i)
+    expect(@lunch).to match(/tag|woch/i)
   end
 end
